9f3d57482c758b04dccee73da6fbfd6ba3701005
Edward Paulo Ganuelas <epganuelas@gmail.com>
2026-05-09 17:12:03 -0400

    add region dropdown

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,23 @@
+import { Picker } from "@react-native-picker/picker";
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
-import { getRegions } from "./utils/api";
+import { ScrollView, Text, View } from "react-native";
+import { getPokeDex, getRegions } from "./utils/api";
+
+interface Pokedex {
+  descriptions: Array<Object>;
+  id: number;
+  is_main_series: boolean;
+  name: string;
+  names: Array<Object>;
+  pokemon_entries: Array<Object>;
+  region: Object;
+}
 
 export default function Index() {
   const [regions, setRegions] = useState<any[]>([]);
+  const [region, setRegion] = useState<Object>("");
+  const [pokedex, setPokedex] = useState<Pokedex>({} as Pokedex);
+
   useEffect(() => {
     getRegions()
       .then((data: any) => {
@@ -15,6 +29,19 @@ export default function Index() {
       });
   }, []);
 
+  function handleRegionChange(itemValue: string) {
+    if (!itemValue) return;
+    setRegion(itemValue);
+    getPokeDex(itemValue)
+      .then((data: any) => {
+        console.log("Pokedex:", data);
+        setPokedex(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching pokedex:", error);
+      });
+  }
+
   return (
     <View
       style={{
@@ -23,13 +50,22 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      {regions.length > 0 ? (
-        regions.map((region: any) => (
-          <Text key={region.name}>{region.name}</Text>
-        ))
-      ) : (
-        <Text>Loading regions...</Text>
-      )}
+      <Picker
+        onValueChange={handleRegionChange}
+        style={{ height: 50, width: 200 }}
+      >
+        <Picker.Item label="Select a region" value={{}} />
+        {regions.map((reg) => (
+          <Picker.Item key={reg.name} label={reg.name} value={reg.url} />
+        ))}
+      </Picker>
+      <ScrollView>
+        {pokedex?.pokemon_entries?.map((pokemon: any) => (
+          <View key={pokemon.entry_number}>
+            <Text>{pokemon.pokemon_species.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
